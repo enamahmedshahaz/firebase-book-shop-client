580a2844227a5ba77fcf9fbeafaa91b9b7aef3b3
enamahmedshahaz <enamahmedshahaz@gmail.com>
2024-09-24 01:15:05 +0600

    Load books data in bookDetails page

--- src/BookDetails/BookDetails.jsx
+++ src/BookDetails/BookDetails.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const BookDetails = () => {
 
     const { bookId } = useParams();
+    const [book, setBook] = useState({});
+
+    useEffect(() => {
+        
+        fetch('/books.json')
+            .then((response) => response.json())
+            .then((books) => {
+                // Find the book that matches the bookId
+                const selectedBook = books.find((b) => b.bookId === parseInt(bookId));
+                setBook(selectedBook);
+
+            });
+    }, [bookId]);
 
     return (
         <div>
-            This is book details page- id -- {bookId}
+            boook name is --  {book.bookName}
         </div>
     );
 };
