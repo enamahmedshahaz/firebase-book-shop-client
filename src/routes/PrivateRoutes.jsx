081c9a689eb22c901490fd01e44849029299ae74
enamahmedshahaz <enamahmedshahaz@gmail.com>
2024-10-08 13:50:44 +0600

    Implement private routes

---
+++ src/routes/PrivateRoutes.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+import PropTypes from 'prop-types';
+
+const PrivateRoutes = ({ children }) => {
+
+    const { user, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <span className="loading loading-spinner loading-lg"></span>;
+    }
+
+    if (user) {
+        return children;
+    }
+    
+    return <Navigate to="/login"></Navigate>;
+    
+};
+
+export default PrivateRoutes;
+
+PrivateRoutes.propTypes = {
+    children: PropTypes.node,
+
+}
